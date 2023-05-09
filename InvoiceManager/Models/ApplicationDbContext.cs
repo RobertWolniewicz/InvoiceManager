91ac27796e0a8f8767bca3ac20c6fc9972658e33
Robert Wolniewicz <robert_wol@wp.pl>
2023-05-09 15:22:20 +0200

    Add project files.

---
+++ InvoiceManager/Models/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using InvoiceManager.Models.Domains;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace InvoiceManager.Models
+{
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoicePosition> InvoicePositions { get; set; }
+        public DbSet<MethodOfPayment> MethodOfPayments { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(au => au.Invoices)
+                .WithRequired(i => i.User)
+                .HasForeignKey(i => i.UserId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(au => au.Clients)
+                .WithRequired(c => c.User)
+                .HasForeignKey(c => c.UserId)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
